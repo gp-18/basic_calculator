cddc9175254d421cd0174d66f93618cfa10dfd6d
gp-18 <parthgupta1803@gmail.com>
2023-02-20 23:02:49 +0530

    Co-authored-by: Gaurav Sharma <alwaysalazycoder@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
-const digit = document.querySelector("#digit");
+
+// https://meet.google.com/kks-jefz-opc join hoja bhai..??
 
 buttons.forEach((item) => {
   item.onclick = () => {
+    // let string2 = display.innerText.toString();
+
+    // if(string2.length >= 8){
+    //   display.innerText="LIMIT EXCEEDED";
+    // }
+    
     if (item.id == "clear") {
       display.innerText = "";
-    } else if (item.id == "backspace") {
+    } 
+    else if (item.id == "backspace") {
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
+    } 
+    else if (display.innerText != "" && item.id == "equal") {
       display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
+    } 
+    else if (display.innerText == "" && item.id == "equal") {
       display.innerText = "Empty!";
       setTimeout(() => (display.innerText = ""), 2000);
-    } else if(string.length()>="8"){
-      digit.style.fontSize = "xx-small";
-    }else {
+    }
+    else{
       display.innerText += item.id;
     }
+
   };
 });
